559b92300e8c7c3ac78eac78df71b8463f51bde8
Kevin <kevin.rodriguez678@gmail.com>
2023-05-15 18:55:09 -0400

    add api call services to call api

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import {TbMovie} from "react-icons/tb"
 const NavBar = () => {
   return ( 
     <header>
-      <TbMovie style={{width: "50px", height:"50px"}}/>
+      <TbMovie style={{width: "20px", height:"20px"}}/>
       <Link to="/movies">Movies</Link>
       <Link to="tv-shows">TV Shows</Link>
       <Link to="search">Search For Movies</Link>
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {RiMovie2Fill} from "react-icons/ri"
+import { getMovies } from "../../services/api-calls"; 
+
 
 const Movies = () => {
-  const [movies, setMovies] = useState(['The Bad Guys', 'The Night House', 'Zootopia'])
+  const [movies, setMovies] = useState([])
+
+  useEffect(()=> {
+    const fetchMovies = async () => {
+      const classData = await getMovies()
+      setMovies(classData.results)
+    }
+    fetchMovies()
+  }, [])
+
   return (
     <>
       <div>
         <h3>Movies</h3>
         <div className="icon-container">
           {movies.map(movieTitle => 
-            <div className="class-div" key={movieTitle}>
+            <div className="class-div" key={movieTitle.index}>
               <RiMovie2Fill style={{width:"20px", height:"20px"}}/>
               {movieTitle}
             </div>
